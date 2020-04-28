bea90f7c4b64d1ed27c9c2f73eeda692d84439dd
clueking1 <wcoan300@gmail.com>
2020-04-28 12:51:10 -0400

    added a simple Navbar and displayed it on the home page

---
+++ src/components/navBar/index.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink } from 'reactstrap';
+
+const Example = () => {
+  const [collapsed, setCollapsed] = useState(true);
+
+  const toggleNavbar = () => setCollapsed(!collapsed);
+
+  return (
+    <div>
+      <Navbar color="faded" light>
+        <NavbarBrand href="/" className="mr-auto">Will Coan</NavbarBrand>
+        <NavbarToggler onClick={toggleNavbar} className="mr-2" />
+        <Collapse isOpen={!collapsed} navbar>
+          <Nav navbar>
+            <NavItem>
+              <NavLink href="/">Home</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="/portfolio">Portfolio</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="/about">About</NavLink>
+            </NavItem>
+          </Nav>
+        </Collapse>
+      </Navbar>
+    </div>
+  );
+}
+
+export default Example;
---
+++ src/pages/home/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import NavBar from '../../components/navBar'
+
+function Home() {
+
+    return(
+        <div className="home">
+            <NavBar />
+        </div>
+    )
+}
+
+export default Home
