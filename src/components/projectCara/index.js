e1c84c98481569ea4be4e04cce02ef4d33a5375d
clueking1 <wcoan300@gmail.com>
2020-05-01 14:52:04 -0400

    added links to project buttons and styled them

--- src/components/projectCara/index.js
+++ src/components/projectCara/index.js
@@ -14,55 +14,55 @@ const Projects = () => {
                 <div>
                     <img src={require('./websitepics/Penny.jpg')} />
                     <button className="gitBut">
-                        <a href="/">Github</a>
+                        <a href="https://github.com/clueking1/Penny-Pinchers" target="_blank">Github</a>
                     </button>
                     <button className="webBut">
-                        <a href="/">Website</a>
+                        <a href="https://ancient-crag-94323.herokuapp.com/" target="_blank">Website</a>
                     </button>
                 </div>
                 <div>
                     <img src={require('./websitepics/Planner.jpg')} />
                     <button className="gitBut">
-                        <a href="/">Github</a>
+                        <a href="https://github.com/clueking1/Day-Planner" target="_blank">Github</a>
                     </button>
                     <button className="webBut">
-                        <a href="/">Website</a>
+                        <a href="https://clueking1.github.io/Day-Planner/" target="_blank">Website</a>
                     </button>
                 </div>
                 <div>
                     <img src={require('./websitepics/Pw.jpg')} />
                     <button className="gitBut">
-                        <a href="/">Github</a>
+                        <a href="https://github.com/clueking1/Password-Generator" target="_blank">Github</a>
                     </button>
                     <button className="webBut">
-                        <a href="/">Website</a>
+                        <a href="https://clueking1.github.io/Password-Generator/" target="_blank">Website</a>
                     </button>
                 </div>
                 <div>
                     <img src={require('./websitepics/WeatherDash.jpg')} />
                     <button className="gitBut">
-                        <a href="/">Github</a>
+                        <a href="https://github.com/clueking1/Weather-API" target="_blank">Github</a>
                     </button>
                     <button className="webBut">
-                        <a href="/">Website</a>
+                        <a href="https://clueking1.github.io/Weather-API/" target="_blank">Website</a>
                     </button>
                 </div>
                 <div>
                     <img src={require('./websitepics/Zodiac.jpg')} />
                     <button className="gitBut">
-                        <a href="/">Github</a>
+                        <a href="https://github.com/clueking1/zodiac-astrology" target="_blank">Github</a>
                     </button>
                     <button className="webBut">
-                        <a href="/">Website</a>
+                        <a href="https://clueking1.github.io/zodiac-astrology/" target="_blank">Website</a>
                     </button>
                 </div>
                 <div>
                     <img src={require('./websitepics/Quiz.jpg')} />
                     <button className="gitBut">
-                        <a href="/">Github</a>
+                        <a href="https://github.com/clueking1/Homework-Quiz" target="_blank">Github</a>
                     </button>
                     <button className="webBut">
-                        <a href="/">Website</a>
+                        <a href="https://clueking1.github.io/Homework-Quiz/" target="_blank">Website</a>
                     </button>
                 </div>
             </Carousel>
