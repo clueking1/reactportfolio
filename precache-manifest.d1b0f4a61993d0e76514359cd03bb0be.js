62d7d6e20311da63ccb3ac22bc187883326f0473
clueking1 <wcoan300@gmail.com>
2020-05-01 15:19:09 -0400

    Updates

---
+++ precache-manifest.d1b0f4a61993d0e76514359cd03bb0be.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5240b6a19ce97ae9af32064d696ce769",
+    "url": "/ereactportgolio/index.html"
+  },
+  {
+    "revision": "5370b8a2cc515fec6767",
+    "url": "/ereactportgolio/static/css/2.b0a38356.chunk.css"
+  },
+  {
+    "revision": "9aef51cdd93f08b4215b",
+    "url": "/ereactportgolio/static/css/main.16e08c4e.chunk.css"
+  },
+  {
+    "revision": "5370b8a2cc515fec6767",
+    "url": "/ereactportgolio/static/js/2.d6bc4aa9.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/ereactportgolio/static/js/2.d6bc4aa9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9aef51cdd93f08b4215b",
+    "url": "/ereactportgolio/static/js/main.eb94bcf4.chunk.js"
+  },
+  {
+    "revision": "ae420fd13114dd87f9b9",
+    "url": "/ereactportgolio/static/js/runtime-main.34bf7971.js"
+  },
+  {
+    "revision": "dbee313f8b5c0a3950691eadf6e52b73",
+    "url": "/ereactportgolio/static/media/Penny.dbee313f.jpg"
+  },
+  {
+    "revision": "ba6c08eeffd332d21f0e875a3a46f0f2",
+    "url": "/ereactportgolio/static/media/Planner.ba6c08ee.jpg"
+  },
+  {
+    "revision": "5f0deff13245d2fdd3552516e8dff8ef",
+    "url": "/ereactportgolio/static/media/Pw.5f0deff1.jpg"
+  },
+  {
+    "revision": "35abe57a012b21c7ded162a0f23f702b",
+    "url": "/ereactportgolio/static/media/Quiz.35abe57a.jpg"
+  },
+  {
+    "revision": "b124a4286d42a25e49259f800c2df8ac",
+    "url": "/ereactportgolio/static/media/WeatherDash.b124a428.jpg"
+  },
+  {
+    "revision": "5c1e63a15f44cbe50f0b10675a3bdc4d",
+    "url": "/ereactportgolio/static/media/Zodiac.5c1e63a1.jpg"
+  },
+  {
+    "revision": "e5b52ee5763f62e383e450fbd71b0abc",
+    "url": "/ereactportgolio/static/media/home4.e5b52ee5.png"
+  },
+  {
+    "revision": "af3233c0aabab586362ffde5da1dd33b",
+    "url": "/ereactportgolio/static/media/smile.af3233c0.jpeg"
+  }
+]);
