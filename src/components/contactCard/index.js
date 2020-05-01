70c747e80b88f49c8d31bad122421b644e8ad343
clueking1 <wcoan300@gmail.com>
2020-05-01 14:35:23 -0400

    social icons added to card

--- src/components/contactCard/index.js
+++ src/components/contactCard/index.js
@@ -11,8 +11,14 @@ function ContactCard(){
       <Card>
         <CardImg top width="100%" src={require('./smile.jpeg')} alt="Card image cap" />
         <CardBody>
-          
-          <Button>Button</Button>
+        <CardSubtitle>Will Coan</CardSubtitle>
+        <CardSubtitle>Full Stack Developer</CardSubtitle>
+          <CardText>Phone: 440-539-7553<br /> Email: Wcoan300@gmail.com</CardText>
+         
+          <div className="tags">
+            <a className="link aTag" href="https://www.linkedin.com/in/will-coan-b65613133/" target='blank'><i class="fa fa-linkedin"></i></a>
+            <a className="git aTag"href="https://github.com/clueking1" target='blank'><i class="fa fa-github"></i></a>
+          </div>
         </CardBody>
       </Card>
     </CardGroup>
